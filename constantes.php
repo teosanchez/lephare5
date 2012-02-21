b603239860d4642278f6046306644fe1952d23d9
unknown <usuario@anfora-9f1965c1.(none)>
2012-02-21 11:08:07 +0100

    jose

--- constantes.php
+++ constantes.php
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 <?php
     define ('SERVIDOR','localhost');
     define ('USUARIO','root');
     define ('PASSWORD','');
     define ('BD','lephare5');
     define ('REGISTROS_PAGINA',10);
-
 ?> 
-=======
-<?php
-    define ('SERVIDOR','localhost');
-    define ('USUARIO','root');
-    define ('PASSWORD','');
-    define ('BD','lephare5');
-    define ('REGISTROS_PAGINA',10);
-?>
->>>>>>> 5f0db35cbdf3417fabc6e2ede1c76b401ff1e046
