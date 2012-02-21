7d008ed23aa1bc0a2a9ea037c1b709dae221eb54
unknown <usuario@anfora-9f1965c1.(none)>
2012-02-21 10:59:07 +0100

    jose

--- constantes.php
+++ constantes.php
@@ -4,30 +4,5 @@
     define ('PASSWORD','');
     define ('BD','lephare5');
     define ('REGISTROS_PAGINA',10);
-<<<<<<< HEAD
-    
-    // Jose antonio!!
-=======
-<<<<<<< HEAD
-    //Nando deja de chatear por Facebook 9:06 Ernesto Lephare loko!
-=======
-<<<<<<< HEAD
-    //Andaluza de Formularios Tropicales SA 09.21
-=======
-<<<<<<< HEAD
-    /* Carmen 9:06 */
-=======
-<<<<<<< HEAD
-    //modif rafa 09:06 21_02_2012
-=======
-<<<<<<< HEAD
-    //Manuel 9:06
-=======
-    //prueba TEO 9:02
->>>>>>> bf9decade891f78e2b1e9122dc2e7883f301fc27
->>>>>>> f0df179469eb5f0525886f36662f04db3ce03a5d
->>>>>>> 1f82005c4b8b26966a3acdeb91fc5d12f2daa63a
->>>>>>> 050ce2d7e1030fc5742a6599f589d1bf3653e08b
->>>>>>> e6626cdbda5b351d4fbe2290840486230e4243bc
->>>>>>> 8ef1537b125c0ecd290ee2db5e8144fb26e09a70
-?>
+
+?> 
