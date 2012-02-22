68c8b480f4eb3e548b22d929f7ece121b53ba4c6
unknown <Usuario@anfora-436c6222.(none)>
2012-02-22 09:41:00 +0100

    Paginacion 9:40

--- rejilla_visitas.php
+++ rejilla_visitas.php
@@ -1,6 +1,7 @@
 <?php
 include ("clase_rejilla.php");
 include_once ("clase_bd.php");
+include_once ("clase_paginador.php");
 $bd = new bd();
 
 /* * ********* Establecer consulta ************** */
@@ -9,27 +10,24 @@ $fecha = "";
 $result = "";
 $result2 = "";
 /* * ********* Paginacion ************** */
-$registros = 2;
-$inicio = 0;
-if (isset($_GET['pagina']))
+if(!isset($_GET['ipp']))
     {
-    $pagina = $_GET['pagina'];
-    $inicio = ($pagina - 1) * $registros;
-    } 
-else 
-    {
-    $pagina = 1;
+    $_GET['ipp']='';
     }
-$resultados = $bd->consultar("SELECT * FROM vw_rejilla_visitas");
-
-$total_registros = mysql_num_rows($resultados);
-$total_paginas = ceil($total_registros / $registros);
+$result2=$bd->consultarArray("SELECT * FROM vw_rejilla_visitas");
+$num_registros=count($result2);
+$pages= new Paginator;
+$pages->items_total=$num_registros;
+$pages->paginate();
 /* * ********* Fin Paginacion ************** */
 if (isset($_GET["fecha"]) && $_GET["fecha"] <> "")
     {
     $fecha = $_GET["fecha"];
-    $result = $bd->consultarArray("select * from vw_rejilla_visitas where Fecha='" . $fecha . "'");
-    $result2 = $bd->consultar("select * from vw_rejilla_visitas where Fecha='" . $fecha . "'");
+    $result = $bd->consultarArray("select * from vw_rejilla_visitas where Fecha='" . $fecha . "' $pages->limit");
+    $result2 = $bd->consultarArray("select * from vw_rejilla_visitas where Fecha='" . $fecha . "'");
+    $num_registros=count($result2); 
+    $pages->items_total=$num_registros;
+    $pages->paginate();
     } 
 else
     {
@@ -37,19 +35,23 @@ else
         {    
         $cadena = $_GET["cadena"];
         $result = $bd->consultarArray("SELECT * from vw_rejilla_visitas
+        where Paciente like '%" . $cadena . "%' or Medico like '%" . $cadena . "%' $pages->limit");
+        $result2 = $bd->consultarArray("SELECT * from vw_rejilla_visitas
         where Paciente like '%" . $cadena . "%' or Medico like '%" . $cadena . "%'");
-        $result2 = $bd->consultar("SELECT * from vw_rejilla_visitas
-        where Paciente like '%" . $cadena . "%' or Medico like '%" . $cadena . "%'");
+        $num_registros=count($result2); 
+        $pages->items_total=$num_registros;
+        $pages->paginate();
         }   
     else 
         {    
         if (!isset($_GET["buscar_fecha"]) and !isset($_GET["buscar_cadena"]))
             {        
             /* paginacion (ordenado por fecha) */
-            $result = $bd->consultarArray("SELECT * FROM vw_rejilla_visitas ORDER BY Fecha asc LIMIT $inicio, $registros");
+            $result = $bd->consultarArray("SELECT * FROM vw_rejilla_visitas ORDER BY Fecha asc  $pages->limit");
             }
         }
     }
+echo '<h3>REJILLA VISITAS</h3><br/>';    
 
 if ($result)
     {
@@ -57,14 +59,13 @@ if ($result)
     echo $rejilla->pintar();
     if ($result2 <> "") /* Incluir  en generador este if */ 
         {  
-        $num_registros = mysql_num_rows($result2);
         if ($num_registros == 1)
             {
-            echo '<p>Se ha encontrado ' . $num_registros . ' registro.</p>';
+            echo '<br/>Se ha encontrado ' . $num_registros . ' registro.';
             }
         else 
             {        
-            echo '<p>Se han encontrado ' . $num_registros . ' registros.</p>';
+            echo '<br/>Se han encontrado ' . $num_registros . ' registros.';
             }
         }
     } 
@@ -73,17 +74,15 @@ else /* Incluir en generador este else */
     if (isset($_GET["buscar_fecha"]) && $fecha == "")
         {
         echo '<p class="error">Introduzca una fecha.</p>';
+        $num_registros='';
         } 
     else
         {
         if (isset($_GET["buscar_cadena"]) && $cadena == "") 
             {       
             echo '<p class="error">Introduzca el dato que desea buscar.</p>';
+            $num_registros='';
             } 
-        else
-            {
-            echo '<p class="error">No se ha encontrado ningún registro.</p>';
-            }
         }
     }
 
@@ -92,31 +91,21 @@ if (isset($_GET['msj2']) && $_GET['msj2'] != "")   //Incluir en Generador
     echo '<p>' . $_GET['msj2'] . '</p>';            //Incluir en Generador
     } //Incluir en Generador
 /* * ********* Paginacion ************** */
-if (($pagina - 1) > 0)
+if($num_registros>10)
     {
-    echo "<a href='index.php?cuerpo=rejilla_visitas.php&pagina=" . ($pagina - 1) . "'>< Anterior</a> ";
+    echo '&nbsp;&nbsp;';
+    echo $pages->display_jump_menu();
+    echo '&nbsp;&nbsp;';
+    echo $pages->display_items_per_page();
+    echo '&nbsp;&nbsp;';
+    echo "Pagina: $pages->current_page de $pages->num_pages";
     }
-for ($i = 1; $i <= $total_paginas; $i++)
+if($num_registros==0)
     {
-    if ($pagina == $i)
-        {
-        echo "<b>" . $pagina . "</b> ";
-        } 
-    else
-        {
-        echo "<a href='index.php?cuerpo=rejilla_visitas.php&pagina=$i'>$i</a>&nbsp;";
-        }
-    }
-if (($pagina + 1) <= $total_paginas)
-    {
-    echo " <a href='index.php?cuerpo=rejilla_visitas.php&pagina=" . ($pagina + 1) . "'>Siguiente ></a>";
-    }
+    echo "No se ha encontrado ningun registro.";
+    }   
 /* * ********* Fin Paginacion ************** */
 ?>
-<div class="titulo">
-    <h3>VISITAS</h3>
-</div>
-
 <div class="buscar">
     <form action="index.php" method="get">       
         <input type="text" name="cadena"/>
