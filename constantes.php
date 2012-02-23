38cb9c9840124788c2ea64d26e3cc657bdf136ba
unknown <Usuario@anfora-436c6222.(none)>
2012-02-23 12:54:17 +0100

    Paginacion

--- constantes.php
+++ constantes.php
@@ -2,6 +2,5 @@
     define ('SERVIDOR','localhost');
     define ('USUARIO','root');
     define ('PASSWORD','');
-    define ('BD','lephare5');
-    define ('REGISTROS_PAGINA',10);
+    define ('BD','lephare5');    
 ?> 
